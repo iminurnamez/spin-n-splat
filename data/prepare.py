21ff50c267a8f00bb81b14c67135317589ebee25
iminurnamez <easinerf@gmail.com>
2016-03-28 17:40:09 -0400

    removed unneeded asset dicts

--- data/prepare.py
+++ data/prepare.py
@@ -1,7 +1,7 @@
 import os
 import pygame as pg
 from . import tools
-#from .components import players
+
 
 SCREEN_SIZE = (1080, 740)
 ORIGINAL_CAPTION = "Spin 'n Splat"
@@ -13,7 +13,4 @@ SCREEN = pg.display.set_mode(SCREEN_SIZE)
 SCREEN_RECT = SCREEN.get_rect()
 
 
-FONTS = tools.load_all_fonts(os.path.join("resources", "fonts"))
-MUSIC = tools.load_all_music(os.path.join("resources", "music"))
-SFX   = tools.load_all_sfx(os.path.join("resources", "sound"))
 GFX   = tools.load_all_gfx(os.path.join("resources", "graphics"))
